84d02536f87f6d92c7920b3b2169e30641abc0b2
GaoYisheng <1160139211@qq.com>
2017-07-02 20:52:09 +0800

    完善01真假的测试： if [ ] 和 [ ]

--- trueOrNot.sh
+++ trueOrNot.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # 0 1 true or not example :
 
-
+# 测试 if 判断语句
 if [ 1 ];then
 	echo "1 says \" Hello world ! \" "
 fi 
@@ -9,4 +9,20 @@ fi
 if [ 0 ];then
 	echo "0 says \" Hello world ! \" "
 fi 
+
+# 测试 整数关系
+i1=10
+i2=15
+i3=10
+
+echo "10==10"
+[ $i1 -eq $i3 ]
+echo $?
+
+echo "10==15"
+[ $i1 -eq $i2 ]
+echo $?
+
+
+
 
