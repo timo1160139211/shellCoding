4a4ad27bcd0c90a54913d325b78427ffd5a5dcff
GaoYisheng <1160139211@qq.com>
2017-08-12 11:31:32 +0800

    shell loop-for

---
+++ forLoop.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# loop - for
+
+######## syntax :#########
+# for [var] in [list]    #
+# do                     #
+#     command0           #
+#     command1           #
+#     ...                #
+# done                   #
+##########################
+
+# ls $HOME
+for FILE in $HOME/*
+do 
+	echo $FILE
+done
+
+# int list
+for var in 1 2 3 4 5 6 7 8
+do
+	echo "this is the No.$var line"
+done	
