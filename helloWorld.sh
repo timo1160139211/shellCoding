40eee6d8836b759b9bc1aee60e0dc51252076324
Gao Yisheng <1160139211@qq.com>
2017-09-05 21:08:47 +0800

    daily

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 # a simple shell script example :
 # a say hello function:
+
+
+##########################
+#       function         #
+##########################
+
 function say_hello(){
 	echo -n "Please Enter Your Name :"
 	read name
 	echo "$name : hello world! "
 }
+
+
+###########################
+#          main           #
+###########################
 echo "The Programme Start Here ..."
 say_hello
 echo "The Programme Ends ."
